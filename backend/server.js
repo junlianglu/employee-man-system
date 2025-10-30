97548749a72cbc1294f3e88f7a1484057d20fbc6
Junyu Lu <141600678+junyulu463@users.noreply.github.com>
2025-10-29 18:24:18 -0700

    fix dotenv should be import first before app.js in server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+import 'dotenv/config';
 import app from "./app.js";
-import dotenv from "dotenv";
 import mongoDB from "./config/db.js";
 
-dotenv.config();
+//dotenv.config();
 mongoDB();
 app.listen(process.env.PORT || 5001, () => {console.log(`connect server successfully`)});
